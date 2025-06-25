f174e06f5f6f1fd73d7c32520d56380912cc4b99
lorinbaum <lorin.baumgarten@outlook.com>
2025-06-25 19:04:51 +0200

    hack: reusable bitmap writer

--- ascii_glyphatlas.py
+++ ascii_glyphatlas.py
@@ -1,45 +1,8 @@
 import struct, math
-from ttf import Interpreter, vec2
+from spiritstream.font.ttf import TTF
+from spiritstream.vec import vec2
+from typing import Iterable
 
-I =  Interpreter(fontfile="assets/fonts/Fira_Code_v6.2/ttf/FiraCode-Regular.ttf")
-
-# render ascii set
-width = 16
-# glyph_count = I.maxp.numGlyphs # all glyphs
-glyph_count = 128 # all glyphs
-fontsize = 10
-dpi = 150
-I.debug = False
-
-glyphs = []
-for i in range(glyph_count):
-    I.getGlyph(i, fontsize=fontsize, dpi=dpi, antialiasing=3)
-    glyphs.append({
-        "bitmap": I.g.bitmap.copy(),
-        "bearing": vec2(I.g.bearing.x, I.g.bearing.y), # recreate object?
-        "width": len(I.g.bitmap[0]) if len(I.g.bitmap) else 0,
-        "height": len(I.g.bitmap)
-    })
-
-minX, maxX = min([g["bearing"].x for g in glyphs]), max([g["bearing"].x + g["width"] for g in glyphs])
-minY, maxY = min([g["bearing"].y - g["height"] for g in glyphs]), max([g["bearing"].y for g in glyphs])
-tile = vec2( # big enough for every glyph
-    maxX - minX,
-    maxY - minY
-)
-origin = vec2(-minX, minY) # relative to bottom left corner. y is because in array indexing increasing y points down
-
-bmp = [[[0,0,0] for _ in range(tile.x * width)] for _ in range(tile.y * math.ceil(glyph_count/width))]
-
-for i, g in enumerate(glyphs):
-    tile_bottom_left_corner = vec2(tile.x * (i % width), tile.y * (i // width + 1))
-    bearing = vec2(g["bearing"].x, - g["bearing"].y) # y is because in array indexing increasing y points down
-    glyph_top_left_corner = tile_bottom_left_corner + origin + bearing
-    for y, row in enumerate(g["bitmap"]):
-        bmp_y = glyph_top_left_corner.y + y
-        for x, px in enumerate(row):
-            bmp_x = glyph_top_left_corner.x + x
-            bmp[bmp_y][bmp_x] = [int(px*255), 0, 0]
 
 def write_bmp(filename, pixels):
     """Write a 24-bit BMP from a 2D array of RGB tuples.
@@ -78,8 +41,12 @@ def write_bmp(filename, pixels):
     pixel_data = bytearray()
     
     for row in reversed(pixels):  # BMPs are stored bottom-to-top
-        for r, g, b in row:
-            pixel_data.extend([b, g, r])  # BMP uses BGR order
+        # if len(row) == 3:
+        #     for r, g, b in row:
+        #         pixel_data.extend([b, g, r])  # BMP uses BGR order
+        #     pixel_data.extend(b'\x00' * row_padding)
+        # else:
+        for v in row: pixel_data.extend([v, v, v])
         pixel_data.extend(b'\x00' * row_padding)
     
     with open(filename, 'wb') as f:
@@ -87,4 +54,46 @@ def write_bmp(filename, pixels):
         f.write(dib_header)
         f.write(pixel_data)
 
-write_bmp("ascii.bmp", bmp)
+
+if __name__ == "__main__":
+    I =  TTF(fontfile="assets/fonts/Fira_Code_v6.2/ttf/FiraCode-Regular.ttf")
+
+    # render ascii set
+    width = 16
+    # glyph_count = I.maxp.numGlyphs # all glyphs
+    glyph_count = 128 # all glyphs
+    fontsize = 10
+    dpi = 150
+    I.debug = False
+
+    glyphs = []
+    for i in range(glyph_count):
+        I.getGlyph(i, fontsize=fontsize, dpi=dpi, antialiasing=3)
+        glyphs.append({
+            "bitmap": I.g.bitmap.copy(),
+            "bearing": vec2(I.g.bearing.x, I.g.bearing.y), # recreate object?
+            "width": len(I.g.bitmap[0]) if len(I.g.bitmap) else 0,
+            "height": len(I.g.bitmap)
+        })
+
+    minX, maxX = min([g["bearing"].x for g in glyphs]), max([g["bearing"].x + g["width"] for g in glyphs])
+    minY, maxY = min([g["bearing"].y - g["height"] for g in glyphs]), max([g["bearing"].y for g in glyphs])
+    tile = vec2( # big enough for every glyph
+        maxX - minX,
+        maxY - minY
+    )
+    origin = vec2(-minX, minY) # relative to bottom left corner. y is because in array indexing increasing y points down
+
+    bmp = [[[0,0,0] for _ in range(tile.x * width)] for _ in range(tile.y * math.ceil(glyph_count/width))]
+
+    for i, g in enumerate(glyphs):
+        tile_bottom_left_corner = vec2(tile.x * (i % width), tile.y * (i // width + 1))
+        bearing = vec2(g["bearing"].x, - g["bearing"].y) # y is because in array indexing increasing y points down
+        glyph_top_left_corner = tile_bottom_left_corner + origin + bearing
+        for y, row in enumerate(g["bitmap"]):
+            bmp_y = glyph_top_left_corner.y + y
+            for x, px in enumerate(row):
+                bmp_x = glyph_top_left_corner.x + x
+                bmp[bmp_y][bmp_x] = [int(px*255), 0, 0]        
+    write_bmp("ascii.bmp", bmp)
+
